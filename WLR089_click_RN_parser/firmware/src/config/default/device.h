5693bbd6542c9bf576ab23b719e71d20f49c893d
farkask <afarkas@chipcad.hu>
2024-04-09 12:11:16 +0200

    modify samr34 header to xc.h

--- WLR089_click_RN_parser/firmware/src/config/default/device.h
+++ WLR089_click_RN_parser/firmware/src/config/default/device.h
@@ -57,7 +57,7 @@
 #ifndef DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
     #define DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
 #endif //DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
-#include "samr34j18b.h"
+#include <xc.h>
 #pragma GCC diagnostic pop
 #include "device_cache.h"
 #include "toolchain_specifics.h"
